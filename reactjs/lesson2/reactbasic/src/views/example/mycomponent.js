3b7ca7cf33c19208556f7f4e5603b8e7e8c24440
dungnguyendn189 <84318162+dungnguyendn189@users.noreply.github.com>
2023-12-13 08:23:20 +0700

    Pushing code learn react
    push

--- reactjs/lesson2/reactbasic/src/views/example/mycomponent.js
+++ reactjs/lesson2/reactbasic/src/views/example/mycomponent.js
@@ -1,35 +1,94 @@
 import React from "react";
+import ChildComponents from "./ChildComponent";
+//bọc 2 thẻ div trong một khối code
+//   <React.Fragment>
+//     <input
+//       value={this.state.name}
+//       type="text"
+//       onChange={(event) => this.handleOnchange(event)}
+//     />
+
+//     <div className="fist">
+//       Hello My Component {this.state.name}
+//       {/* {console.log(" My Name is : ", name)} */}
+//     </div>
+//     <div className="second"> BBB</div>
+//     <div className="third">
+//       <button onClick={this.handleClickButton}>Click Me</button>
+//     </div>
+//   </React.Fragment>
+// );
+//   }
+
+// state = {
+//     name: "Dung Map",
+//     channel: "Nha Ther",
+//   };
+//   handleOnchange = (event) => {
+//     console.log(event.target.value);
+//     this.setState({
+//       name: event.target.value,
+//     });
+//   };
+
+//   handleClickButton = () => {
+//     console.log("Hit The Button");
+//     alert("Click me");
+//   };
+
+//   render() {
+//     let name = "Dũng";
 
 class MyComponent extends React.Component {
   state = {
-    name: "Dung Map",
-    channel: "Nha Ther",
+    firstName: "",
+    lastname: "",
   };
-  handleOnchange = (event) => {
-    console.log(event);
+
+  handleOnchangeFirstName = (event) => {
     this.setState({
-      name: event.target.value,
+      firstName: event.target.value,
     });
   };
 
+  handleOnchangeLastName = (event) => {
+    this.setState({
+      lastname: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Data : ", this.state);
+  };
+
   render() {
-    let name = "Dũng";
-
     return (
-      //bọc 2 thẻ div trong một khối code
-      <React.Fragment>
+      <form>
+        <label for="fname">First name:</label>
+        <br />
         <input
-          value={this.state.name}
           type="text"
-          onChange={(event) => this.handleOnchange(event)}
+          value={this.state.firstName}
+          onChange={(event) => this.handleOnchangeFirstName(event)}
         />
-
-        <div className="fist">
-          Hello My Component {this.state.name}
-          {/* {console.log(" My Name is : ", name)} */}
-        </div>
-        <div className="second"> BBB</div>
-      </React.Fragment>
+        <br />
+        <label for="lname">Last name:</label>
+        <br />
+        <input
+          type="text"
+          value={this.state.lastname}
+          onChange={(event) => this.handleOnchangeLastName(event)}
+        />
+        <br />
+        <br />
+        <input
+          type="button"
+          value="Submit"
+          onClick={(event) => this.handleSubmit(event)}
+        />
+        <ChildComponents />
+      </form>
     );
   }
 }
