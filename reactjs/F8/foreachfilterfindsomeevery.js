2d17e3f849311ae44d3ab9d0d30d3c30a48f94fc
dungnguyendn189 <84318162+dungnguyendn189@users.noreply.github.com>
2024-01-02 16:22:46 +0700

    update tutorial

---
+++ reactjs/F8/foreachfilterfindsomeevery.js
@@ -0,0 +1,42 @@
+var names = ["JS", "PHP"];
+
+names.length = 10;
+
+// for (var i = 0; i < names.length; i++) {
+//   console.log(names[i]);
+// }
+
+var forEachNames = names.forEach((e) => {
+  console.log(e);
+});
+
+//filter
+
+var names2 = [
+  {
+    name: "PHP",
+    coin: 300,
+  },
+  {
+    name: "JS",
+    coin: 400,
+  },
+  {
+    name: "Dart",
+    coin: 440,
+  },
+  {
+    name: "React",
+    coin: 600,
+  },
+  {
+    name: "ReactJS",
+    coin: 980,
+  },
+];
+
+var filterName2 = names2.filter((e) => {
+  return e.coin > 500;
+});
+
+console.log(filterName2);
