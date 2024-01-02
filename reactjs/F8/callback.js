ec7279720635ece6f1a2ac9b890ba9dea7015222
dungnguyendn189 <84318162+dungnguyendn189@users.noreply.github.com>
2024-01-02 15:50:42 +0700

    Pushing code callback

--- reactjs/F8/callback.js
+++ reactjs/F8/callback.js
@@ -9,3 +9,15 @@ function myCallback(value) {
 }
 
 MyFunction(myCallback);
+
+// callback là hàm
+// truyền qua đối số
+// được gọi lại ( trong hàm nhận đối số)
+
+var courses = ["Javascript", "php", "Dart"];
+
+var htmls = courses.map((e) => {
+  return `<h2>${e}</h2>`;
+});
+
+console.log(htmls.join(""));
