185263c53b88fef29eab0ce03281a8cc2dad48e0
dungnguyendn189 <84318162+dungnguyendn189@users.noreply.github.com>
2024-01-02 15:22:10 +0700

    update callback
    Update JS callback

---
+++ reactjs/F8/includes.js
@@ -0,0 +1,10 @@
+// console.log(Array.prototype.includes);
+
+var title = "Responsive Web Design";
+
+console.log(title.includes("Web Design"));
+console.log(title.includes("Responsive", 1));
+
+var courses = ["Javascript", "php", "Dart"];
+
+console.log(courses.includes("php"));
