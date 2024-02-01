a1b7df76446673247ea5da39c597f6c4b5013510
dungnguyendn189 <84318162+dungnguyendn189@users.noreply.github.com>
2024-02-01 17:01:07 +0700

    Update Source UseRedeucer
     Warning Del

--- reactjs/F8_New/my-app/src/App.js
+++ reactjs/F8_New/my-app/src/App.js
@@ -1,40 +1,93 @@
-import { useState, useReducer } from "react";
+import { useState, useReducer, useRef } from "react";
 // const gifts = ["CUP I9", "Ram 32GB RGB", "RGB Keyboard"];
 
-const init = 0;
-const UP_STATE = "up";
-const DOWN_STATE = "down";
+const initState = {
+  job: "",
+  jobs: [],
+};
 
-const reduce = (states, action) => {
-  switch (action) {
-    case UP_STATE:
-      return states + 1;
-    case DOWN_STATE:
-      return states - 1;
+const SET_JOB = "set_job";
+const ADD_JOB = "add_job";
+const DEL_JOB = "del_job";
+
+const setJob = (payload) => {
+  return {
+    type: SET_JOB,
+    payload,
+  };
+};
+
+const addJob = (payload) => {
+  return {
+    type: ADD_JOB,
+    payload,
+  };
+};
+
+const delJob = (payload) => {
+  return {
+    type: DEL_JOB,
+    payload,
+  };
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_JOB:
+      return {
+        ...state,
+        job: action.payload,
+      };
+      break;
+    case ADD_JOB:
+      return {
+        ...state,
+        jobs: [...state.jobs, action.payload],
+      };
+      break;
+    case DEL_JOB:
+      return;
+      const newJobs = [...state.jobs];
+      const newState = newJobs.splice(action.payload, 1);
+      return {
+        ...state,
+        jobs: newState,
+      };
+
+      break;
     default:
-      throw new Error("Lỗi Rồi");
+      throw new Error("Invalid action.");
   }
 };
 
 function App() {
-  const [states, action] = useReducer(reduce, init);
+  const [state, dispath] = useReducer(reducer, initState);
+  const { job, jobs } = state;
+  const inputRef = useRef();
+  const handleAddJob = () => {
+    dispath(addJob(job));
+    dispath(setJob(""));
+  };
+
   return (
-    <div style={{ alignItems: "center", textAlign: "center", padding: 20 }}>
-      <h1>{states}</h1>
-      <button
-        onClick={() => {
-          action(UP_STATE);
+    <div style={{ padding: 20 }}>
+      <h3>Todo App</h3>
+      <input
+        ref={inputRef}
+        value={job}
+        placeholder="Enter To do"
+        onChange={(e) => {
+          dispath(setJob(e.target.value));
         }}
-      >
-        UP
-      </button>
-      <button
-        onClick={() => {
-          action(DOWN_STATE);
-        }}
-      >
-        Down
-      </button>
+      />
+      <button onClick={handleAddJob}>Add</button>
+      <ul>
+        {jobs.map((job, index) => (
+          <li key={index}>
+            {job} <button onclick={() => dispath(deletedJob(index))}>x</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
